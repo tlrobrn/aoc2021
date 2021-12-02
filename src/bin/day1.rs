2bc6ae359989ef1a7386706b3ca006e0c6cb9cab
Taylor O'Brien <obrien.taylorm@gmail.com>
2021-12-01 22:18:29 -0700

    day1 part2

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -3,9 +3,11 @@ extern crate aoc2021;
 use aoc2021::{input_lines, parse_lines};
 
 fn main() {
-    let input = parse_lines(input_lines());
+    let input: Vec<u64> = parse_lines(input_lines()).collect();
+    let result2 = part2(&input);
     let result = part1(input);
     println!("Part 1: {}", result);
+    println!("Part 2: {}", result2);
 }
 
 fn part1<I>(depths: I) -> u64
@@ -25,14 +27,36 @@ where
     count
 }
 
+fn part2(depths: &[u64]) -> u64 {
+    let mut count = 0;
+    let mut prev = None;
+    for depth_window in depths.windows(3) {
+        let sum = depth_window[0] + depth_window[1] + depth_window[2];
+        if let Some(prev_sum) = prev {
+            if prev_sum < sum {
+                count += 1;
+            }
+        }
+        prev = Some(sum);
+    }
+    count
+}
+
 #[cfg(test)]
 mod day1_tests {
     use super::*;
 
+    const EXAMPLE_INPUT: [u64; 10] = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+
     #[test]
     fn part1_example() {
-        let input = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
-        let result = part1(input);
+        let result = part1(EXAMPLE_INPUT);
         assert_eq!(result, 7);
+    }
+
+    #[test]
+    fn part2_example() {
+        let result = part2(&EXAMPLE_INPUT);
+        assert_eq!(result, 5);
     }
 }
