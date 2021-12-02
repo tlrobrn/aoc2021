f39db8ac6cf1b7dc4554b52fcee064b56783e20d
Taylor O'Brien <obrien.taylorm@gmail.com>
2021-12-01 23:09:23 -0700

    day2 part2

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -3,6 +3,7 @@ extern crate aoc2021;
 use aoc2021::{input_lines, parse_lines};
 use std::str::FromStr;
 
+#[derive(Clone)]
 enum Command {
     Forward(i64),
     Down(i64),
@@ -30,6 +31,8 @@ fn main() {
     let input: Vec<Command> = parse_lines(input_lines()).collect();
     let result = part1(&input);
     println!("Part 1: {}", result);
+    let result = part2(&input);
+    println!("Part 2: {}", result);
 }
 
 fn part1(commands: &[Command]) -> i64 {
@@ -42,6 +45,52 @@ fn part1(commands: &[Command]) -> i64 {
                 Command::Up(units) => (hp, d - units),
             });
     horizontal_position * depth
+}
+
+#[derive(Default)]
+struct Heading {
+    depth: i64,
+    horizontal_position: i64,
+    aim: i64,
+}
+
+impl Heading {
+    fn down(&self, units: i64) -> Self {
+        Self {
+            depth: self.depth,
+            horizontal_position: self.horizontal_position,
+            aim: self.aim + units,
+        }
+    }
+
+    fn up(&self, units: i64) -> Self {
+        Self {
+            depth: self.depth,
+            horizontal_position: self.horizontal_position,
+            aim: self.aim - units,
+        }
+    }
+
+    fn forward(&self, units: i64) -> Self {
+        Self {
+            depth: self.depth + (self.aim * units),
+            horizontal_position: self.horizontal_position + units,
+            aim: self.aim,
+        }
+    }
+}
+
+fn part2(commands: &[Command]) -> i64 {
+    let heading =
+        commands
+            .iter()
+            .cloned()
+            .fold(Heading::default(), |heading, command| match command {
+                Command::Forward(units) => heading.forward(units),
+                Command::Down(units) => heading.down(units),
+                Command::Up(units) => heading.up(units),
+            });
+    heading.horizontal_position * heading.depth
 }
 
 #[cfg(test)]
@@ -61,5 +110,11 @@ mod day2_tests {
     fn part1_example() {
         let result = part1(&EXAMPLE_INPUT);
         assert_eq!(result, 150);
+    }
+
+    #[test]
+    fn part2_example() {
+        let result = part2(&EXAMPLE_INPUT);
+        assert_eq!(result, 900);
     }
 }
