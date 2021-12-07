c042c6bc99ea35f3052dbb6ad93114d9f4c3fc96
Taylor O'Brien <obrien.taylorm@gmail.com>
2021-12-06 21:37:35 -0700

    day 6 part 2

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -1,53 +1,58 @@
 #![warn(clippy::all, clippy::pedantic)]
-use aoc2021::input_lines;
-use std::num::ParseIntError;
+use aoc2021::{input_lines, parse_lines};
 use std::str::FromStr;
 
 fn main() {
-    let mut initial_state = parse();
-    let result = part1(&mut initial_state, 80);
+    let population: LanternFishPopulation = parse_lines(input_lines()).next().expect("no fishes");
+    let result = part1(population);
     println!("Part 1: {}", result);
+    let result = part2(population);
+    println!("Part 2: {}", result);
 }
 
-fn parse() -> Vec<LanternFish> {
-    if let Some(line) = input_lines().next() {
-        line.trim().split(',').map(|f| f.parse().unwrap()).collect()
-    } else {
-        vec![]
-    }
+fn part1(mut population: LanternFishPopulation) -> u128 {
+    population.live(80).count()
 }
 
-fn part1(school: &mut Vec<LanternFish>, days: usize) -> usize {
-    for _day in 1..=days {
-        for i in 0..school.len() {
-            if let Some(child) = school[i].mature() {
-                school.push(child);
-            }
-        }
-    }
-    school.len()
+fn part2(mut population: LanternFishPopulation) -> u128 {
+    population.live(256).count()
 }
 
-#[derive(Debug, Clone, Copy, PartialEq)]
-struct LanternFish(usize);
+#[derive(Debug, Clone, Copy, Default)]
+struct LanternFishPopulation([u128; 9]);
 
-impl FromStr for LanternFish {
-    type Err = ParseIntError;
+impl FromStr for LanternFishPopulation {
+    type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(LanternFish(usize::from_str(s)?))
+        let fishes = s
+            .trim()
+            .split(',')
+            .map(|age| usize::from_str(age).expect("wtf"));
+
+        let mut population = Self::default();
+        for age in fishes {
+            population.0[age] += 1;
+        }
+        Ok(population)
     }
 }
 
-impl LanternFish {
-    fn mature(&mut self) -> Option<Self> {
-        if self.0 == 0 {
-            self.0 = 6;
-            Some(LanternFish(8))
-        } else {
-            self.0 -= 1;
-            None
+impl LanternFishPopulation {
+    fn count(&self) -> u128 {
+        self.0.iter().sum()
+    }
+
+    fn age(&mut self) {
+        self.0.rotate_left(1);
+        self.0[6] += self.0[8];
+    }
+
+    fn live(&mut self, days: usize) -> &mut Self {
+        for _day in 1..=days {
+            self.age();
         }
+        self
     }
 }
 
@@ -55,25 +60,28 @@ impl LanternFish {
 mod day6_tests {
     use super::*;
 
-    fn initial_state() -> Vec<LanternFish> {
-        vec![
-            LanternFish(3),
-            LanternFish(4),
-            LanternFish(3),
-            LanternFish(1),
-            LanternFish(2),
-        ]
+    fn initial_state() -> LanternFishPopulation {
+        LanternFishPopulation([0, 1, 1, 2, 1, 0, 0, 0, 0])
     }
 
     #[test]
-    fn test_part1_short() {
-        let result = part1(&mut initial_state(), 18);
+    fn test_live_short() {
+        let mut pop = initial_state();
+        let result = pop.live(18).count();
         assert_eq!(result, 26);
     }
 
     #[test]
-    fn test_part1_long() {
-        let result = part1(&mut initial_state(), 80);
+    fn test_live_long() {
+        let mut pop = initial_state();
+        let result = pop.live(80).count();
         assert_eq!(result, 5934);
+    }
+
+    #[test]
+    fn test_live_really_long() {
+        let mut pop = initial_state();
+        let result = pop.live(256).count();
+        assert_eq!(result, 26984457539);
     }
 }
