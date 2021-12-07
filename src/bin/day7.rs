f53175ad2d6206945348584873613f0e69dbe406
Taylor O'Brien <obrien.taylorm@gmail.com>
2021-12-07 16:17:04 -0700

    day 7 part 2

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -8,6 +8,8 @@ fn main() {
     let positions: CrabPositions = parse_lines(input_lines()).next().expect("no crabs?");
     let result = part1(&positions);
     println!("Part 1: {}", result);
+    let result = part2(&positions);
+    println!("Part 2: {}", result);
 }
 
 fn part1(positions: &CrabPositions) -> usize {
@@ -17,6 +19,21 @@ fn part1(positions: &CrabPositions) -> usize {
 
     for position in min..=max {
         let cost = positions.cost_to_align_at(position);
+        if cost < minimum_cost {
+            minimum_cost = cost;
+        }
+    }
+
+    minimum_cost
+}
+
+fn part2(positions: &CrabPositions) -> usize {
+    let min = *positions.0.keys().min().expect("please");
+    let max = *positions.0.keys().max().expect("please");
+    let mut minimum_cost = usize::MAX;
+
+    for position in min..=max {
+        let cost = positions.accurate_cost_to_align_at(position);
         if cost < minimum_cost {
             minimum_cost = cost;
         }
@@ -52,6 +69,14 @@ impl CrabPositions {
             sum + position.abs_diff(*crab_position) * crab_count
         })
     }
+
+    fn accurate_cost_to_align_at(&self, position: usize) -> usize {
+        self.0.iter().fold(0, |sum, (crab_position, crab_count)| {
+            let steps = position.abs_diff(*crab_position);
+            let cost = steps * (steps + 1) / 2;
+            sum + cost * crab_count
+        })
+    }
 }
 
 #[cfg(test)]
@@ -64,5 +89,11 @@ mod day6_tests {
     fn test_part1() {
         let result = part1(&CrabPositions::from_str(INPUT).unwrap());
         assert_eq!(result, 37);
+    }
+
+    #[test]
+    fn test_part2() {
+        let result = part2(&CrabPositions::from_str(INPUT).unwrap());
+        assert_eq!(result, 168);
     }
 }
